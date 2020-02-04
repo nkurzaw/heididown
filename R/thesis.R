37771ccf4ab7718f6988ea62796248e651430ee2
Nils Kurzawa <nils.kurzawa@embl.de>
2020-02-04 10:48:44 +0100

    added master thesis template and accompanying function according to IPMB template

--- R/thesis.R
+++ R/thesis.R
@@ -98,6 +98,57 @@ thesis_without_chapter_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "d
     return(base)
 }
 
+#' Creates an R Markdown PDF Thesis document without chapters for submission
+#' of a bachelor/master thesis to IPMB
+#'
+#' This is a function called in output in the YAML of the driver Rmd file to
+#' specify using the Heidelberg University template.
+#'
+#' @export
+#' @param toc A Boolean (TRUE or FALSE) specifying whether table of contents
+#' should be created
+#' @param toc_depth A positive integer
+#' @param highlight Syntax highlighting style. Supported styles include
+#' "default", "tango", "pygments", "kate", "monochrome", "espresso", "zenburn",
+#' and "haddock". Pass NULL to prevent syntax highlighting.
+#' @param ... Additional parameters to pass to `pdf_book()`.
+#' @return A modified \code{pdf_document} based on the HU Berlin School of
+#' Business and Economics LaTeX template.
+#' @examples
+#' \dontrun{
+#'  output: heididown::thesis_without_chapter_ipmb_pdf
+#' }
+thesis_without_chapter_ipmb_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "default", ...) {
+
+    base <- bookdown::pdf_book(
+        template    = "template_wo_chap_ipmb.tex",
+        toc         = toc,
+        toc_depth   = toc_depth,
+        highlight   = highlight,
+        keep_tex    = TRUE,
+        pandoc_args = "--top-level-division=section",
+        ...
+    )
+
+    # Mostly copied from knitr::render_sweave
+    base$knitr$opts_chunk$comment   <- NA
+
+    # To ensure images are in correct place (in line with text)
+    base$knitr$opts_chunk$fig.align <- "center"
+    # base$knitr$opts_chunk$fig.pos    <- "H"
+    # base$knitr$opts_chunk$out.extra  <- ""
+
+    # For tables
+    options(knitr.table.format = "latex")
+    options(kableExtra.latex.load_packages = FALSE)
+
+    old_opt <- getOption("bookdown.post.latex")
+    options(bookdown.post.latex = fix_envs)
+    on.exit(options(bookdown.post.late = old_opt))
+
+    return(base)
+}
+
 fix_envs = function(x) {
     beg_reg <- '^\\s*\\\\begin\\{.*\\}'
     end_reg <- '^\\s*\\\\end\\{.*\\}'
