326fc45ea0625a5c676c72c16cc1e7f3e73663d1
Nils Kurzawa <nils.kurzawa@embl.de>
2021-12-08 09:55:47 +0100

    added function to compile thesis w/o chapters in 2021 style

--- R/thesis.R
+++ R/thesis.R
@@ -148,6 +148,56 @@ thesis_without_chapter_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "d
     return(base)
 }
 
+#' Creates an R Markdown PDF Thesis document with the new style from 2021 without chapters
+#'
+#' This is a function called in output in the YAML of the driver Rmd file to
+#' specify using the Heidelberg University template.
+#'
+#' @export
+#' @param toc A Boolean (TRUE or FALSE) specifying whether table of contents
+#' should be created
+#' @param toc_depth A positive integer
+#' @param highlight Syntax highlighting style. Supported styles include
+#' "default", "tango", "pygments", "kate", "monochrome", "espresso", "zenburn",
+#' and "haddock". Pass NULL to prevent syntax highlighting.
+#' @param ... Additional parameters to pass to `pdf_book()`.
+#' @return A modified \code{pdf_document} based on the HU Berlin School of
+#' Business and Economics LaTeX template.
+#' @examples
+#' \dontrun{
+#'  output: heididown::thesis_without_chapter_2021_pdf
+#' }
+thesis_without_chapter_2021_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "default", ...) {
+
+    base <- bookdown::pdf_book(
+        template    = "template_wo_chap_2021.tex",
+        toc         = toc,
+        toc_depth   = toc_depth,
+        highlight   = highlight,
+        keep_tex    = TRUE,
+        pandoc_args = "--top-level-division=section",
+        ...
+    )
+
+    # Mostly copied from knitr::render_sweave
+    base$knitr$opts_chunk$comment   <- NA
+
+    # To ensure images are in correct place (in line with text)
+    base$knitr$opts_chunk$fig.align <- "center"
+    # base$knitr$opts_chunk$fig.pos    <- "H"
+    # base$knitr$opts_chunk$out.extra  <- ""
+
+    # For tables
+    options(knitr.table.format = "latex")
+    options(kableExtra.latex.load_packages = FALSE)
+
+    old_opt <- getOption("bookdown.post.latex")
+    options(bookdown.post.latex = fix_envs)
+    on.exit(options(bookdown.post.late = old_opt))
+
+    return(base)
+}
+
 #' Creates an R Markdown PDF Thesis document without chapters for submission
 #' of a bachelor/master thesis to IPMB
 #'
