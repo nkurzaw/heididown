dd71fc48d1c9f29c1ce62dd5f64850c33daace0d
Nils Kurzawa <nils.kurzawa@embl.de>
2021-12-08 09:31:35 +0100

    added new function to compile thesis based on 2021 template

--- R/thesis.R
+++ R/thesis.R
@@ -48,6 +48,56 @@ thesis_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "default", ...) {
     return(base)
 }
 
+#' Creates an R Markdown PDF Thesis document with the new style from 2021
+#'
+#' This is a function called in output in the YAML of the driver Rmd file to
+#' specify using the Heidelberg University template.
+#'
+#' @export
+#' @param toc A Boolean (TRUE or FALSE) specifying whether table of contents
+#' should be created
+#' @param toc_depth A positive integer
+#' @param highlight Syntax highlighting style. Supported styles include
+#' "default", "tango", "pygments", "kate", "monochrome", "espresso", "zenburn",
+#' and "haddock". Pass NULL to prevent syntax highlighting.
+#' @param ... Additional parameters to pass to `pdf_book()`.
+#' @return A modified \code{pdf_document} based on the thesisdown template and
+#' the huwiwidown template.
+#' @examples
+#' \dontrun{
+#'  output: heididown::thesis_2021_pdf
+#' }
+thesis_2021_pdf <- function(toc = TRUE, toc_depth = 3, highlight = "default", ...) {
+
+    base <- bookdown::pdf_book(
+        template    = "template_2021.tex",
+        toc         = toc,
+        toc_depth   = toc_depth,
+        highlight   = highlight,
+        keep_tex    = TRUE,
+        pandoc_args = "--top-level-division=chapter",
+        ...
+    )
+
+    # Mostly copied from knitr::render_sweave
+    base$knitr$opts_chunk$comment   <- NA
+
+    # To ensure images are in correct place (in line with text)
+    base$knitr$opts_chunk$fig.align <- "center"
+    # base$knitr$opts_chunk$fig.pos    <- "H"
+    # base$knitr$opts_chunk$out.extra  <- ""
+
+    # For tables
+    options(knitr.table.format = "latex")
+    options(kableExtra.latex.load_packages = FALSE)
+
+    old_opt <- getOption("bookdown.post.latex")
+    options(bookdown.post.latex = fix_envs)
+    on.exit(options(bookdown.post.late = old_opt))
+
+    return(base)
+}
+
 #' Creates an R Markdown PDF Thesis document without chapters
 #'
 #' This is a function called in output in the YAML of the driver Rmd file to
